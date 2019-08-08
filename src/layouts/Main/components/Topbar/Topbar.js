dadf3cb2bd95102854311b90158cdf20766e9a85
yayanheeh <tokoxp58@gmail.com>
2019-08-08 17:51:11 +0700

    Update Topbar.js

--- src/layouts/Main/components/Topbar/Topbar.js
+++ src/layouts/Main/components/Topbar/Topbar.js
@@ -36,7 +36,7 @@ const Topbar = props => {
         <RouterLink to="/">
           <img
             alt="Logo"
-            src="/images/logos/logo--white.svg"
+            src="/images/garangan.png"
           />
         </RouterLink>
         <div className={classes.flexGrow} />
