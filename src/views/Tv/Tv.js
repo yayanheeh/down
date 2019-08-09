e2bccb4d7c76f0c166d2b31fe9dedc5b0138d60b
yayanheeh <tokoxp58@gmail.com>
2019-08-09 13:13:08 +0700

    Update Tv.js

--- src/views/Tv/Tv.js
+++ src/views/Tv/Tv.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
+import { Grid } from '@material-ui/core';
+
+import { Tv, TvList } from './components';
 
 const useStyles = makeStyles(theme => ({
   root: {
     padding: theme.spacing(4)
-  },
-  iframe: {
-    width: '100%',
-    minHeight: 640,
-    border: 0
   }
 }));
 
@@ -17,11 +15,29 @@ const Tv = () => {
 
   return (
     <div className={classes.root}>
-      <iframe
-        className={classes.iframe}
-        src="http://ytdl.ezyro.com"
-        title="Ytd"
-      />
+      <Grid
+        container
+        spacing={4}
+      >
+        <Grid
+          item
+          lg={4}
+          md={6}
+          xl={4}
+          xs={12}
+        >
+          <Tv />
+        </Grid>
+        <Grid
+          item
+          lg={8}
+          md={6}
+          xl={8}
+          xs={12}
+        >
+          <TvList />
+        </Grid>
+      </Grid>
     </div>
   );
 };
