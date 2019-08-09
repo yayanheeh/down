adeaec89c1bcb235ed1e6055d19f460b9430f26a
yayanheeh <tokoxp58@gmail.com>
2019-08-09 16:37:15 +0700

    Update Yayan.js

--- src/views/Yayan/Yayan.js
+++ src/views/Yayan/Yayan.js
@@ -1,41 +1,111 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import moment from 'moment';
 import { makeStyles } from '@material-ui/styles';
-import { Grid } from '@material-ui/core';
-
-import { View, List } from './components';
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Avatar,
+  Typography,
+  Divider,
+  Button,
+  LinearProgress
+} from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    padding: theme.spacing(4)
+  root: {},
+  details: {
+    display: 'flex'
+  },
+  avatar: {
+    marginLeft: 'auto',
+    height: 110,
+    width: 100,
+    flexShrink: 0,
+    flexGrow: 0
+  },
+  progress: {
+    marginTop: theme.spacing(2)
+  },
+  uploadButton: {
+    marginRight: theme.spacing(2)
   }
 }));
 
-const Yayan = () => {
+const Yayan = props => {
+  const { className, ...rest } = props;
+
   const classes = useStyles();
 
+  const user = {
+    name: 'Shen Zhi',
+    city: 'Los Angeles',
+    country: 'USA',
+    timezone: 'GTM-7',
+    avatar: '/images/avatars/avatar_11.png'
+  };
+
   return (
-    <div className={classes.root}>
-      <Grid
-        container
-        spacing={4}
-      >
-        <Grid
-          item
-          md={7}
-          xs={12}
+    <Card
+      {...rest}
+      className={clsx(classes.root, className)}
+    >
+      <CardContent>
+        <div className={classes.details}>
+          <div>
+            <Typography
+              gutterBottom
+              variant="h2"
+            >
+              John Doe
+            </Typography>
+            <Typography
+              className={classes.locationText}
+              color="textSecondary"
+              variant="body1"
+            >
+              {user.city}, {user.country}
+            </Typography>
+            <Typography
+              className={classes.dateText}
+              color="textSecondary"
+              variant="body1"
+            >
+              {moment().format('hh:mm A')} ({user.timezone})
+            </Typography>
+          </div>
+          <Avatar
+            className={classes.avatar}
+            src={user.avatar}
+          />
+        </div>
+        <div className={classes.progress}>
+          <Typography variant="body1">Profile Completeness: 70%</Typography>
+          <LinearProgress
+            value={70}
+            variant="determinate"
+          />
+        </div>
+      </CardContent>
+      <Divider />
+      <CardActions>
+        <Button
+          className={classes.uploadButton}
+          color="primary"
+          variant="text"
         >
-          <View />
-        </Grid>
-        <Grid
-          item
-          md={5}
-          xs={12}
-        >
-          <List />
-        </Grid>
-      </Grid>
-    </div>
+          Upload picture
+        </Button>
+        <Button variant="text">Remove picture</Button>
+      </CardActions>
+    </Card>
   );
+};
+
+Yayan.propTypes = {
+  className: PropTypes.string
 };
 
 export default Yayan;
