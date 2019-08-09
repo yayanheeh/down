f59c36075ad02ac94ed50bf4f8eb7611f4e0f973
yayanheeh <tokoxp58@gmail.com>
2019-08-09 15:08:37 +0700

    Update Yayan.js

--- src/views/Yayan/Yayan.js
+++ src/views/Yayan/Yayan.js
@@ -38,29 +38,27 @@ const Yayan = () => {
 
   return (
     <div className={classes.root}>
-        <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        <Grid
+        container
+        spacing={4}
+      >
+<Grid
+          item
+          md={5}
+          xs={12}
+        >
+          Tes
         </Grid>
       </Grid>
+
+        <Grid
+          item
+          md={7}
+          xs={12}
+        >
+          Tes2
+        </Grid>
+        
     </div>
   );
 };
