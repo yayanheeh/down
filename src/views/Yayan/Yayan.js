956755eca3ca50f97d2656e6e37fbe1160a241a3
yayanheeh <tokoxp58@gmail.com>
2019-08-09 14:48:38 +0700

    Update Yayan.js

--- src/views/Yayan/Yayan.js
+++ src/views/Yayan/Yayan.js
@@ -6,7 +6,12 @@ import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    padding: theme.spacing(4)
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
   }
 }));
 
@@ -35,7 +40,7 @@ const Yayan = () => {
     <div className={classes.root}>
       <Grid
         container
-        spacing={4}
+        spacing={3}
       >
         <Grid item xs={12} sm={6}>
           <Paper className={classes.paper}>xs=12 sm=6</Paper>
