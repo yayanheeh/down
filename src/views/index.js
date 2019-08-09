14df9d506679f7e0b82e36e42ec31120e2a3729e
yayanheeh <tokoxp58@gmail.com>
2019-08-09 14:30:41 +0700

    Update index.js

--- src/views/index.js
+++ src/views/index.js
@@ -7,5 +7,7 @@ export { default as Settings } from './Settings';
 export { default as SignIn } from './SignIn';
 export { default as SignUp } from './SignUp';
 export { default as Typography } from './Typography';
+export { default as Yayan } from './Yayan';
+
 export { default as UserList } from './UserList';
 export { default as Tv } from './Tv';
