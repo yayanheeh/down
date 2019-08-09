11fffe49b96a697013b4f3d0c2b9e50cc4792ace
yayanheeh <tokoxp58@gmail.com>
2019-08-09 15:11:22 +0700

    Update Yayan.js

--- src/views/Yayan/Yayan.js
+++ src/views/Yayan/Yayan.js
@@ -6,12 +6,7 @@ import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
+    padding: theme.spacing(4)
   }
 }));
 
